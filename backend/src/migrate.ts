b7c248cbc9fc025527b5d2da64c2f7deb3a1f5bc
aliraza <alirazanodelays@gmail.com>
2026-04-13 16:36:21 +0500

    fixed

--- backend/src/migrate.ts
+++ backend/src/migrate.ts
@@ -5,6 +5,9 @@ import { Pool } from "pg";
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false    // 👈 fix
+    }
 });
 
 const db = drizzle(pool);
